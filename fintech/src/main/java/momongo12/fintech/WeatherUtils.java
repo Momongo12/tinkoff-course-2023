8d0b31780a9d29fa756aaa067fce8e15400fd2bd
Momongo12 <den.moskvin2045@mail.ru>
2023-09-24 17:28:21 +0700

    added the WeatherUtils class with signatures of methods representing job solutions

---
+++ fintech/src/main/java/momongo12/fintech/WeatherUtils.java
@@ -0,0 +1,31 @@
+package momongo12.fintech;
+
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Momongo12
+ * @version 1.0
+ */
+public class WeatherUtils {
+
+    private WeatherUtils() {}
+
+    public static double calculateAverageTemperature(List<Weather> weathersList) {
+        return 0.0;
+    }
+
+    public static List<String> findRegionsWithTemperatureLargerSome(List<Weather> weatherList, double temperature) {
+        return null;
+    }
+
+    public static Map<Integer, List<Double>> convertToMapGroupingByRegionId(List<Weather> weatherList) {
+        return null;
+    }
+
+    public static Map<Integer, Collection<Weather>> convertToMapGroupingByTemperature(List<Weather> weathersList) {
+        return null;
+    }
+}
