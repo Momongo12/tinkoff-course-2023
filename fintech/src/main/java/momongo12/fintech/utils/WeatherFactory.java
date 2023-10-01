69a1a91aa81c24a88260ba7b7aca19610b1a6970
Momongo12 <den.moskvin2045@mail.ru>
2023-10-02 02:35:35 +0700

    feat: added a method to create Weather in WeatherFactory

--- fintech/src/main/java/momongo12/fintech/utils/WeatherFactory.java
+++ fintech/src/main/java/momongo12/fintech/utils/WeatherFactory.java
@@ -12,7 +12,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author Momongo12
- * @version 1.1
+ * @version 1.2
  */
 @Component
 @NoArgsConstructor
@@ -25,19 +25,33 @@ public class WeatherFactory {
      * Creates a new Weather object for the specified region and temperature value.
      * @param regionName
      * @param temperatureValue
+     * @param measuringDate
+     * @return Weather
+     * @implNote The region identifier (regionId) will be determined based on the regionName and will be unique for each unique regionName.
+     * @implNote measuringDate will be initialized with the current time stamp
+     * @apiNote This method is thread-safe
+     */
+    public Weather createWeather(String regionName, double temperatureValue, Instant measuringDate) {
+        return Weather
+                .builder()
+                .regionId(getRegionIdByRegionName(regionName))
+                .regionName(regionName)
+                .temperatureValue(temperatureValue)
+                .measuringDate(measuringDate)
+                .build();
+    }
+
+    /**
+     * Creates a new Weather object for the specified region and temperature value.
+     * @param regionName
+     * @param temperatureValue
      * @return Weather
      * @implNote The region identifier (regionId) will be determined based on the regionName and will be unique for each unique regionName.
      * @implNote measuringDate will be initialized with the current time stamp
      * @apiNote This method is thread-safe
      */
     public Weather createWeather(String regionName, double temperatureValue) {
-        return Weather
-                .builder()
-                .regionId(getRegionIdByRegionName(regionName))
-                .regionName(regionName)
-                .temperatureValue(temperatureValue)
-                .measuringDate(Instant.now())
-                .build();
+        return createWeather(regionName, temperatureValue, Instant.now());
     }
 
     public int getRegionIdByRegionName(String regionName) {
