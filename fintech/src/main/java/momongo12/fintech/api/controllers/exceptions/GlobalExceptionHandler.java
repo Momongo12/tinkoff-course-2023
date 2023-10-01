57cf6a1f41cbce135544e8617e051b3205b8c736
Momongo12 <den.moskvin2045@mail.ru>
2023-10-01 23:46:13 +0700

    feat: added GlobalExceptionHandler for add info about exception from controller methods at response body

---
+++ fintech/src/main/java/momongo12/fintech/api/controllers/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package momongo12.fintech.api.controllers.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleResponseStatusException(NotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(DuplicateResourceException.class)
+    public ResponseEntity<String> handleDuplicateResourceException(DuplicateResourceException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
+}
