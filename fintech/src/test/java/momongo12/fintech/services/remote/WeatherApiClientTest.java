7eeb99d1baedf5a7ceb89a519a21c1eecd60d886
Momongo12 <den.moskvin2045@mail.ru>
2023-10-30 05:23:04 +0700

    test: added stub to WeatherApiClientTest

--- fintech/src/test/java/momongo12/fintech/services/remote/WeatherApiClientTest.java
+++ fintech/src/test/java/momongo12/fintech/services/remote/WeatherApiClientTest.java
@@ -1,68 +1,77 @@
 package momongo12.fintech.services.remote;
 
 import momongo12.fintech.api.dto.WeatherApiResponse;
-import momongo12.fintech.services.exceptions.WeatherApiTokenKeyException;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Optional;
 
 /**
  * @author Momongo12
- * @version 1.0
+ * @version 2.0
  */
-@ExtendWith(MockitoExtension.class)
 public class WeatherApiClientTest {
 
-    @Mock
-    private RestTemplate restTemplate;
-
-    @InjectMocks
     private WeatherApiClient weatherApiClient;
+    private StubRestTemplate restTemplate;
+
+    @BeforeEach
+    void setUp() {
+        restTemplate = new StubRestTemplate();
+        weatherApiClient = new WeatherApiClient(restTemplate);
+    }
 
     @Test
     void testGetCurrentWeatherWithValidRegionName() {
         String validRegionName = "kurgan";
-        WeatherApiResponse mockResponse = new WeatherApiResponse();
-        ResponseEntity<WeatherApiResponse> responseEntity = ResponseEntity.ok(mockResponse);
-        when(restTemplate.exchange(anyString(), eq(HttpMethod.GET), isNull(), eq(WeatherApiResponse.class)))
-                .thenReturn(responseEntity);
+        WeatherApiResponse stubResponse = new WeatherApiResponse();
+        restTemplate.setStubResponse(stubResponse);
 
         Optional<WeatherApiResponse> weatherApiResponse = weatherApiClient.getCurrentWeather(validRegionName);
 
         assertTrue(weatherApiResponse.isPresent());
-        assertEquals(mockResponse, weatherApiResponse.get());
-        verify(restTemplate, times(1)).exchange(anyString(), eq(HttpMethod.GET), isNull(), eq(WeatherApiResponse.class));
+        assertEquals(stubResponse, weatherApiResponse.get());
     }
 
     @Test
     void testGetCurrentWeatherWithInvalidRegionName() {
         assertThrows(IllegalArgumentException.class, () -> weatherApiClient.getCurrentWeather(null));
-
-        verify(restTemplate, never()).exchange(anyString(), eq(HttpMethod.GET), isNull(), eq(WeatherApiResponse.class));
     }
 
     @Test
     void testGetCurrentWeatherWithApiError() {
         String validRegionName = "kurgan";
-        when(restTemplate.exchange(anyString(), eq(HttpMethod.GET), isNull(), eq(WeatherApiResponse.class)))
-                .thenThrow(new WeatherApiTokenKeyException("API token error"));
+        restTemplate.setStubResponse(null);
 
         Optional<WeatherApiResponse> weatherApiResponse = weatherApiClient.getCurrentWeather(validRegionName);
 
         assertFalse(weatherApiResponse.isPresent());
-        verify(restTemplate, times(1)).exchange(anyString(), eq(HttpMethod.GET), isNull(), eq(WeatherApiResponse.class));
+    }
+
+    private static class StubRestTemplate extends RestTemplate {
+
+        private WeatherApiResponse stubResponse;
+
+        public void setStubResponse(WeatherApiResponse stubResponse) {
+            this.stubResponse = stubResponse;
+        }
+
+        @Override
+        public <T> ResponseEntity<T> exchange(String url, HttpMethod method, HttpEntity<?> requestEntity, Class<T> responseType, Object... uriVariables)
+                throws RestClientException {
+            if (responseType.isAssignableFrom(WeatherApiResponse.class)) {
+                return ResponseEntity.ok((T) stubResponse);
+            }
+            return super.exchange(url, method, requestEntity, responseType, uriVariables);
+        }
     }
 }
